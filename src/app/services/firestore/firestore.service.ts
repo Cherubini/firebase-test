3f08352b264279e38e8e3f83d8e701cf1c2f02e8
Cherubini <127298157+Cherubini@users.noreply.github.com>
2023-06-09 00:28:40 +0200

    added user interface

--- src/app/services/firestore/firestore.service.ts
+++ src/app/services/firestore/firestore.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { initializeApp } from "firebase/app";
 import { Firestore, collection, getDocs, getFirestore } from "firebase/firestore";
 import { doc, getDoc } from "firebase/firestore";
 import { Manga } from '../../model/manga';
